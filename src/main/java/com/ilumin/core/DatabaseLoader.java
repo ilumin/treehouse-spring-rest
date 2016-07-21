84cb76717c7cd7814a0ea951e448a2d7f5990e93
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-07-21 11:07:20 +0700

    ADD example data in data loader

--- src/main/java/com/ilumin/core/DatabaseLoader.java
+++ src/main/java/com/ilumin/core/DatabaseLoader.java
@@ -8,6 +8,10 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 @Component
 public class DatabaseLoader implements ApplicationRunner {
 
@@ -20,11 +24,32 @@ public class DatabaseLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Course course = new Course("ilumin 101", "http://ilumin-101.com");
-        course.addReview(new Review(5, "Yikes"));
-        course.addReview(new Review(5, "Yikes Yikes"));
-        course.addReview(new Review(5, "Yikes Yikes Yikes"));
-        courses.save(course);
+        exampleData();
+    }
+
+    private void exampleData() {
+        String[] templates = {
+                "Yikes %s %d",
+                "Yep %s %d",
+                "Yeah %s %d"
+        };
+        String[] buzzworlds = {
+                "Spring",
+                "Java",
+                "Scala"
+        };
+
+        List<Course> bunchOfCourse = new ArrayList<>();
+        IntStream.range(0, 100)
+                .forEach(i -> {
+                    String template = templates[i % templates.length];
+                    String buzzworld = buzzworlds[i % buzzworlds.length];
+                    String title = String.format(template, buzzworld, i);
+                    Course c = new Course(title, "http://ilumin-101.com");
+                    c.addReview(new Review(i % 5, String.format("More %s %s", buzzworld, i)));
+                    bunchOfCourse.add(c);
+                });
+        courses.save(bunchOfCourse);
     }
 
 }
