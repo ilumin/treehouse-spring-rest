3f489b6e29868fbd6957edd834777fe2cf574d40
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-07-21 13:48:48 +0700

    ADD user relation with review

--- src/main/java/com/ilumin/review/Review.java
+++ src/main/java/com/ilumin/review/Review.java
@@ -2,6 +2,7 @@ package com.ilumin.review;
 
 import com.ilumin.core.BaseEntity;
 import com.ilumin.course.Course;
+import com.ilumin.user.User;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
@@ -14,6 +15,9 @@ public class Review extends BaseEntity {
 
     @ManyToOne
     private Course course;
+
+    @ManyToOne
+    private User reviewer;
 
     protected Review() {
         super();
@@ -46,5 +50,13 @@ public class Review extends BaseEntity {
 
     public void setCourse(Course course) {
         this.course = course;
+    }
+
+    public User getReviewer() {
+        return reviewer;
+    }
+
+    public void setReviewer(User reviewer) {
+        this.reviewer = reviewer;
     }
 }
