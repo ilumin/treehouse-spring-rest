bd6b8c6153009d0e6290bd2c54892944e7bf5e54
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-07-21 11:19:49 +0700

    ADD custom search method in course repository

--- src/main/java/com/ilumin/course/CourseRepository.java
+++ src/main/java/com/ilumin/course/CourseRepository.java
@@ -1,6 +1,14 @@
 package com.ilumin.course;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
+
+    @RestResource(rel = "title-contains", path = "contain-title")
+    Page<Course> findByTitleContaining(@Param("title") String title, Pageable page);
+
 }
